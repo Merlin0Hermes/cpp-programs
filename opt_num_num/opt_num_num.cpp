512bc7d375607e8dd5d280f0ae0f4d198f0e90bb
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-14 17:55:41 +0530

    add code

--- opt_num_num/opt_num_num.cpp
+++ opt_num_num/opt_num_num.cpp
@@ -4,14 +4,29 @@
 
 int main()
 {
-	char opt;
+	string opt;
 	double a, b;
-	cout << "Enter operations and two numbers: ";
+
+	cout << "Enter operation and two numbers: ";
 	cin >> opt >> a >> b;
 
+	if (opt == "+" || opt == "plus")
+	{
+		cout << a << " + " << b << " = " << a + b;
+	}
+	else if (opt == "-" || opt == "minus")
+	{
+		cout << a << " - " << b << " = " << a - b;
+	}
+	else if (opt == "*" || opt == "mul")
+	{
+		cout << a << "*" << b << " = " << a * b;
+	}
+	else if (opt == "/" || opt == "div")
+	{
+		cout << a << "/" << b << " = " << a / b;
+	}
 
-
-
-
+	return 0;
 
 }
