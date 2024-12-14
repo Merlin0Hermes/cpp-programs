4e8e4ead9518d51c8b8dc8920d2ef88b76657e91
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-14 14:29:10 +0530

    add conditonal for friend's sex

--- letter_to_friend.cpp/letter_to_friend.cpp.cpp
+++ letter_to_friend.cpp/letter_to_friend.cpp.cpp
@@ -9,8 +9,17 @@ int main()
 	cout << "Enter the name of the other friend: ";
 	cin >> friend_name;
 
+	char friend_sex = 0;
+	cout << "Enter m if friend is male or f if friend is female\n";
+	cin >> friend_sex;
+
 	cout << "Dear " << first_name << "," << "\n";
 	cout << "\tHow are you? I am fine. I miss you.\n";
 	cout << "Have you seen " << friend_name << " lately?\n";
+	
+	if (friend_sex == 'm')
+		cout << "If you see " << friend_name << " please ask him to call me.\n";
+	else if (friend_sex == 'f')
+		cout << "If you see " << friend_name << " please ask her to call me.\n";
 	
 }
