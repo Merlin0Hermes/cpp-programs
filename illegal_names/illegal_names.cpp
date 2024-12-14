4c4f3ffd4ca6344682c8ba9ae28a3eb350ff4e89
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-14 15:12:05 +0530

    add code

---
+++ illegal_names/illegal_names.cpp
@@ -0,0 +1,20 @@
+// program to demonstrate legal and illegal names in C++
+
+#include "../std_lib_facilities.h"
+
+int main()
+{
+	// legal names
+	int a = 0;
+	int Int = 5;
+	float f = 3.14;
+	char character = 'a';
+	double d = 5.256;
+
+	// illegal names
+	// int double = 5;
+	// float 9u = 7;
+	// char ? = '?';
+	// bool if = true;
+
+}
