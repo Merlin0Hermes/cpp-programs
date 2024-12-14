6d5ddc9d08c2292d8b5ba7dd814179e836bff538
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-14 17:46:18 +0530

    add files and template code

---
+++ opt_num_num/opt_num_num.cpp
@@ -0,0 +1,13 @@
+// program to perform operations in format opt a b and display result a opt b.
+
+#include "../std_lib_facilities.h"
+
+int main()
+{
+	char opt;
+	double a, b;
+	cout << "Enter operations and two numbers: ";
+	cin << opt << a << b;
+
+
+}
