a9ff43c985a219b10ae63972d96c9f21c00497e9
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-14 14:21:19 +0530

    add friend_name input and output

--- letter_to_friend.cpp/letter_to_friend.cpp.cpp
+++ letter_to_friend.cpp/letter_to_friend.cpp.cpp
@@ -2,8 +2,15 @@
 
 int main()
 {
-	string first_name;
-	cout << "Enter the name of the person you want to write to\n ";
+	string first_name, friend_name;
+	cout << "Enter the name of the person you want to write to: ";
 	cin >> first_name;
+	
+	cout << "Enter the name of the other friend: ";
+	cin >> friend_name;
+
 	cout << "Dear " << first_name << "," << "\n";
+	cout << "\tHow are you? I am fine. I miss you.\n";
+	cout << "Have you seen " << friend_name << " lately?\n";
+	
 }
