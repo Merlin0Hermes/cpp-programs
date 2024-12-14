e8e6894b32b76ebb2d1e52345c2e2b1ff706c919
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-14 18:08:14 +0530

    add input from user

---
+++ coin_denominations/coin_denominations.cpp
@@ -0,0 +1,28 @@
+// program to accept and display different coins like pennies, nickels, etc and calculate the total
+
+#include "../std_lib_facilities.h"
+
+int main()
+{
+	int pennies, nickels, dimes, quarters, half_dollars, one_dollars;
+
+	cout << "How many pennies do you have? ";
+	cin >> pennies;
+
+	cout << "How many nickels do you have? ";
+	cin >> nickels;
+
+	cout << "How many dimes do you have? ";
+	cin >> dimes;
+
+	cout <<  "How many quarters do you have? ";
+	cin >> quarters;
+
+	cout << "How many half-dollars do you have? ";
+	cin >> half_dollars;
+
+	cout << "How many one-dollars do you have? ";
+	cin >> one_dollars;
+
+
+}
