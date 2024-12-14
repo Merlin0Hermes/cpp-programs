33854f5854f36033a4146579490fef931dbc6a97
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-14 14:35:54 +0530

    add recepient age input and output

--- letter_to_friend.cpp/letter_to_friend.cpp.cpp
+++ letter_to_friend.cpp/letter_to_friend.cpp.cpp
@@ -3,8 +3,9 @@
 int main()
 {
 	string first_name, friend_name;
-	cout << "Enter the name of the person you want to write to: ";
-	cin >> first_name;
+	int age;
+	cout << "Enter the name and age of the person you want to write to: ";
+	cin >> first_name >> age;
 	
 	cout << "Enter the name of the other friend: ";
 	cin >> friend_name;
@@ -13,7 +14,7 @@ int main()
 	cout << "Enter m if friend is male or f if friend is female\n";
 	cin >> friend_sex;
 
-	cout << "Dear " << first_name << "," << "\n";
+	cout << "\nDear " << first_name << "," << "\n";
 	cout << "\tHow are you? I am fine. I miss you.\n";
 	cout << "Have you seen " << friend_name << " lately?\n";
 	
@@ -21,5 +22,7 @@ int main()
 		cout << "If you see " << friend_name << " please ask him to call me.\n";
 	else if (friend_sex == 'f')
 		cout << "If you see " << friend_name << " please ask her to call me.\n";
+
+	cout << "I hear you just had a birthday and you are " << age << " years old.\n";
 	
 }
