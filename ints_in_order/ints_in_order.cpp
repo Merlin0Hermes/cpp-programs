a1a5e81c204535790b1314d7b7d468fc91f6e5ca
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-14 17:20:08 +0530

    add code

--- ints_in_order/ints_in_order.cpp
+++ ints_in_order/ints_in_order.cpp
@@ -4,5 +4,19 @@
 
 int main()
 {
+	int a, b, c;
+
+	cout << "Enter three integers: ";
+	cin >> a >> b >> c;
+
+	if (a > b)
+		swap(a, b);
+	if (a > c)
+		swap(a, c);
+	if (b > c)
+		swap(b, c);
+
+
+	cout << a << ' ' << b << ' ' << c;
 
 }
