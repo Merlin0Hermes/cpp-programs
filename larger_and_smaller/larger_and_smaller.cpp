6783f0bde1a7b0b4fd032e36cadcc19ba7501ea7
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-15 18:48:14 +0530

    add check to output if the numbers are almost equal

--- larger_and_smaller/larger_and_smaller.cpp
+++ larger_and_smaller/larger_and_smaller.cpp
@@ -15,6 +15,11 @@ int main()
 		{
 			cout << "The smaller value is: " << min(num1, num2) << '\n';
 			cout << "The larger value is: " << max(num1, num2) << '\n';
+
+			if (max(num1, num2) - min(num1, num2) < 1.0 / 100)
+			{
+				cout << "The numbers are almost equal.\n";
+			}
 		}
 
 	}
