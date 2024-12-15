f8b5d236cc22a295e7270eef7dea547dc6f718e7
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-15 22:14:57 +0530

    add more error checking for input

--- sequence_median/sequence_median.cpp
+++ sequence_median/sequence_median.cpp
@@ -15,14 +15,18 @@ int main()
 	double median;
 	int n = numbers.size();
 
-	if (n > 1 && n % 2 == 0)
+	if (n >= 2 && n % 2 == 0)
 	{
 		double sum = numbers[n / 2] + numbers[n / 2 - 1];
 		median = sum / 2;
 	}
-	else
+	else if (n >= 1 && n % 2 != 0)
 	{
 		median = numbers[n / 2];
+	}
+	else
+	{
+		simple_error("Invalid data!\n");
 	}
 
 	cout << "Median is " << median << "\n";
