a67cfe2a495f10ea5e2f25c17a2d410d8dba1ef6
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-15 14:55:39 +0530

    add template code

---
+++ the_first_program/the_first_program.cpp
@@ -0,0 +1,11 @@
+// program to implement square function using repeated addition and print the square of first hundred numbers using that function. 
+
+int square(int n)
+{
+
+}
+
+int main()
+{
+
+}
