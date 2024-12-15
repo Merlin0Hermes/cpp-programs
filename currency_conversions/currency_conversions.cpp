845a256a72bdc5f757174e4501ec00c68a2f97dc
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-15 12:43:49 +0530

    add first iteration of code

--- currency_conversions/currency_conversions.cpp
+++ currency_conversions/currency_conversions.cpp
@@ -4,5 +4,32 @@
 
 int main()
 {
+	constexpr double yen_to_dol = 0.0065;
+	constexpr double euro_to_dol = 1.05;
+	constexpr double pnd_to_dol = 1.26;
 
+	char currency;
+	cout << "Enter if yen, euro, or pound (y, e, p): ";
+	cin >> currency;
+
+	double amount;
+	cout << "Enter the amount of currency: ";
+	cin >> amount;
+
+	if (currency == 'y')
+	{
+		cout << amount << " yen is $" << amount * yen_to_dol << '\n';
+	}
+	else if (currency == 'e')
+	{
+		cout << amount << " euro is $" << amount * euro_to_dol << '\n';
+	}
+	else if (currency == 'p')
+	{
+		cout << amount << " pound is $" << amount * pnd_to_dol << '\n';
+	}
+	else
+	{
+		cout << "I don't know this currency!\n";
+	}
 }
