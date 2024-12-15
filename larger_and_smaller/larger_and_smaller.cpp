4856a975daba56a034f7af5f3ddf6b5f4ed3846e
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-15 19:46:27 +0530

    add unit calculations

--- larger_and_smaller/larger_and_smaller.cpp
+++ larger_and_smaller/larger_and_smaller.cpp
@@ -2,29 +2,38 @@
 
 int main()
 {
+	constexpr double in_to_m = 0.0254;
+	constexpr double ft_to_m = 0.3048;
+	constexpr double cm_to_m = 0.01;
 	double num;
 
 	cout << "Enter an number with unit (cm, m, in, ft)\n";
-	double min_val = INFINITY, max_val = -INFINITY, true_val = 0;
+	double min_val = INFINITY, max_val = -INFINITY, sum = 0;
 	string unit = "";
 
 	while (cin >> num >> unit)
-	{
+	{	
+		double true_val = 0.0;
 		if (unit == "cm")
 		{
-			true_val = num / 100;
+			true_val = num * cm_to_m;
 		}
 		else if (unit == "in")
 		{
-			true_val = (num * 2.54) / 100;
+			true_val = num * in_to_m;
 		}
 		else if (unit == "ft")
 		{
-			true_val = num * 12 * 2.54 / 100;
+			true_val = num * ft_to_m;
+		}
+		else if (unit == "m")
+		{
+			true_val = num;
 		}
 		else
 		{
-			true_val = num;
+			cout << "Unknown unit!\n";
+			continue;
 		}
 
 		if (true_val < min_val)
