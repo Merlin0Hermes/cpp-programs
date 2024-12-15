ae03bcebd0f03d1e2b8e1536c5209aaac6066fb0
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-15 15:00:02 +0530

    add missing header

--- the_first_program/the_first_program.cpp
+++ the_first_program/the_first_program.cpp
@@ -1,5 +1,7 @@
 // program to implement square function using repeated addition and print the square of first hundred numbers using that function. 
 
+#include "../std_lib_facilities.h"
+
 int square(int n)
 {
 	int result = 0;
