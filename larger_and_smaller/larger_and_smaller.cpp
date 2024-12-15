5c059481519ffb0e9aeefb45784173d2a2a8b98c
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-15 18:36:47 +0530

    modify output to print larger and smaller value

--- larger_and_smaller/larger_and_smaller.cpp
+++ larger_and_smaller/larger_and_smaller.cpp
@@ -4,9 +4,11 @@ int main()
 {
 	int num1, num2;
 
+	cout << "Enter two integers: ";
 	while (cin >> num1 >> num2)
 	{
-		cout << num1 << ' ' << num2 << '\n';
+		cout << "the smaller value is: " << min(num1, num2) << '\n';
+		cout << "the larger value is: " << max(num1, num2) << '\n';
 	}
 	
 	return 0;
