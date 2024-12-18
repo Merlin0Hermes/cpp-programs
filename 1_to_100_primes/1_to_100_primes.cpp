76a337a5373b4a3406b31b236802ea17f97e3c29
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-18 20:50:11 +0530

    add function to check for prime numbers

--- 1_to_100_primes/1_to_100_primes.cpp
+++ 1_to_100_primes/1_to_100_primes.cpp
@@ -2,7 +2,39 @@
 
 #include "..\std_lib_facilities.h"
 
+bool is_prime(int n);
+
 int main()
 {
+	const int start = 1, end = 100;
+	vector<int> primes;
 
+	for (int i = start; i <= end; i++)
+	{
+		if (is_prime(i))
+		{
+			primes.push_back(i);
+		}
+	}
+}
+
+bool is_prime(int n)
+{
+	if (n <= 1)
+	{
+		return false;
+	}
+	else if (n != 2 && n % 2 == 0)
+	{
+		return false
+	}
+
+	for (int i = 3; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
 }
