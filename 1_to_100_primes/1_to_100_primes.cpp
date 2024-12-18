e19ab6fea126b2b86da0ce211fe748dc7b34e994
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-18 20:53:50 +0530

    outputs the found prime numbers

--- 1_to_100_primes/1_to_100_primes.cpp
+++ 1_to_100_primes/1_to_100_primes.cpp
@@ -16,6 +16,14 @@ int main()
 			primes.push_back(i);
 		}
 	}
+
+	cout << '\n';
+	for (int x : primes)
+	{
+		cout << x << '\n';
+	}
+
+	return 0;
 }
 
 bool is_prime(int n)
@@ -26,7 +34,7 @@ bool is_prime(int n)
 	}
 	else if (n != 2 && n % 2 == 0)
 	{
-		return false
+		return false;
 	}
 
 	for (int i = 3; i < n; i++)
