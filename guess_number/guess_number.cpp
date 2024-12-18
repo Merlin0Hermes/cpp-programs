352a23fc66d1222c78484ed7c52a2b5a6c10098d
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-18 16:33:50 +0530

    add loop to guess numbers

--- guess_number/guess_number.cpp
+++ guess_number/guess_number.cpp
@@ -4,15 +4,39 @@
 
 int main()
 {
+	int low = 1, high = 100;
 	char answer;
 
-	int low, high;
 	cout << "Think of a number between 1 and 100: ";
 	cout << "\nEnter y or n\n";
 
-	while ()
+	int guess;
+	while (low <= high)
 	{
+		guess = (low + high) / 2;
 
+		cout << "Is you number less than " << guess << "? ";
+		cin >> answer;
+
+		if (answer == 'y')
+		{
+			high = guess - 1;
+		}
+		else if (answer == 'n')
+		{
+			cout << "Is your number " << guess << "? ";
+			cin >> answer;
+			
+			if (answer == 'y')
+			{
+				cout << "Great! I guessed you number :)\n";
+				return 0;
+			}
+			else if (answer == 'n')
+			{
+				low = guess + 1;
+			}
+		}
 	}
 
 }
