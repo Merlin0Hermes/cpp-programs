a595a27d855a83427968032a93cea2a24c04ab20
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-18 16:22:37 +0530

    add first input and template code

---
+++ guess_number/guess_number.cpp
@@ -0,0 +1,18 @@
+// program to guess which number user typed in
+
+#include "../std_lib_facilities.h"
+
+int main()
+{
+	char answer;
+
+	int low, high;
+	cout << "Think of a number between 1 and 100: ";
+	cout << "\nEnter y or n\n";
+
+	while ()
+	{
+
+	}
+
+}
