93ebbd97deadc847f9c1c53d332f7f8da2bcc06d
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-18 17:36:33 +0530

    add files and template code

---
+++ num_words_interchage/num_words_interchage.cpp
@@ -0,0 +1,23 @@
+// program to display digit form if user enters a word and vice versa 
+// example: 7 -> seven and eight -> 8
+
+#include "../std_lib_facilities.h"
+
+int main()
+{
+	
+	const vector<string> words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+	const vector<string> numbers = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+	string input;
+
+	while (cin >> input)
+	{
+		if (input.size() == 1)
+		{
+
+		}
+	}
+
+
+}
