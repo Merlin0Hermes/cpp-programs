cfdb8b5997f5be1858e2cfce9a7d795ce2b59cdf
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-22 21:40:23 +0530

    add main part of input and output

--- rock_paper_scissors/rock_paper_scissors.cpp
+++ rock_paper_scissors/rock_paper_scissors.cpp
@@ -7,40 +7,68 @@ int user_points = 0, machine_points = 0;
 vector<char> win = { 'r', 'p', 's' };
 vector<char> lose = { 's', 'r', 'p' };
 
+void determine_points(char player, char machine);
+
 
 int main()
 {
-	vector<double> random_values;
+	vector<int> random_values;
 
-	cout << "Enter 5 random numbers.";
+	const int max_turns = 5;
+
+	cout << "Enter " << max_turns << " random numbers.\n";
 	cout << "Enter q to exit.\n";
-	for (double temp; cin >> temp; )
+
+	for (int i = 0; i < 5; i++)
 	{
+		int temp;
+		cin >> temp;
 		random_values.push_back(temp);
 	}
+
+	
 	
 
-	string object;
+	string user_choice;
+	int i = 0;
+
 	while (true)
 	{
 		cout << "Enter rock, paper, or scissors: ";
-		cin >> object;
+		cin >> user_choice;
+		char machine_choice = win[random_values[i] % 3];
 
-		switch (object[0])
+
+		switch (user_choice[0])
 		{
 			case 'r':
+				determine_points('r', machine_choice);
+				break;
+			case 'p':
+				determine_points('p', machine_choice);
+				break;
+			case 's':
+				determine_points('s', machine_choice);
+				break;
 
+			default:
+				cout << "Invalid choice!\n";
+				continue;
+		}
+		i++;
 
-
-
+		if (i == max_turns)
+		{
+			break;
 		}
 
 	}
 
+	cout << "Human points: " << user_points << '\n';
+	cout << "Machine points: " << machine_points << '\n';
 
 
 
-	
 
 	return 0;
 }
