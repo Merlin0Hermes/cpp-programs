d940fd52ce590a435ce315a4a012b34f895ccd95
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-24 16:07:54 +0530

    fix formatting

--- errors/errors.cpp
+++ errors/errors.cpp
@@ -4,16 +4,22 @@
 
 
 int main()
-try {
-	<< your code here >>
+{
+	try
+	{
+		// code here
+			keep_window_open();
+		return 0;
+	}
+	catch (exception& e)
+	{
+		cerr << "error: " << e.what() << '\n';
 		keep_window_open();
-} return 0;
-catch (exception& e) {
-	cerr << "error: " << e.what() << '\n';
-	keep_window_open();
-} return 1;
-catch (...) {
-	cerr << "Oops: unknown exception!\n";
-	keep_window_open();
-	return 2;
+		return 1;
+	}
+	catch (...) {
+		cerr << "Oops: unknown exception!\n";
+		keep_window_open();
+		return 2;
+	}
 }
