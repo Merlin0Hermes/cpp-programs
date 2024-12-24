5105ebb476dac618163f958f256851679bfe0bd4
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-24 16:18:21 +0530

    remove redundant code to keep window open

--- errors/errors.cpp
+++ errors/errors.cpp
@@ -8,18 +8,15 @@ int main()
 	try
 	{
 		cout << "Success!\n";
-		keep_window_open();
 		return 0;
 	}
 	catch (exception& e)
 	{
 		cerr << "error: " << e.what() << '\n';
-		keep_window_open();
 		return 1;
 	}
 	catch (...) {
 		cerr << "Oops: unknown exception!\n";
-		keep_window_open();
 		return 2;
 	}
 }
