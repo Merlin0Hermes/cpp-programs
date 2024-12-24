51567bc68f5f2562fa31aa68644f49ab95bc9a66
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-24 16:29:27 +0530

    change from () to [] for subscripting

--- errors/errors.cpp
+++ errors/errors.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	try
 	{
-		vector<int> v(10); v(5) = 7; if (v(5) != 7) cout << "Success!\n";
+		vector<int> v(10); v[5] = 7; if (v[5] != 7) cout << "Success!\n";
 		return 0;
 	}
 	catch (exception& e)
