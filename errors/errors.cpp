531e5b1c926047f4bca4851ef293cc2b2bfc6803
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-24 16:49:42 +0530

    found no actual error other than suspected logical error

--- errors/errors.cpp
+++ errors/errors.cpp
@@ -7,6 +7,7 @@ int main()
 {
 	try
 	{
+		                                          // logical error here? 
 		vector<char> v(5); for (int i = 0; i <= v.size(); ++i); cout << "Success!\n";
 		return 0;
 	}
