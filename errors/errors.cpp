9a9bb24660a393f0afd7b7afed3cf91e52535a34
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-24 17:02:58 +0530

    increment i instead of j

--- errors/errors.cpp
+++ errors/errors.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	try
 	{
-		int i = 0; int j = 9; while (i < 10) ++j; if (j < i) cout << "Success!\n";
+		int i = 0; int j = 9; while (i < 10) ++i; if (j < i) cout << "Success!\n";
 		return 0;
 	}
 	catch (exception& e)
