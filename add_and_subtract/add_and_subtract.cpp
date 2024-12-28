f22ee7b23acd2ce4d51e06e407f8f88400b22e4c
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2024-12-28 17:26:21 +0530

    add prompting user

---
+++ add_and_subtract/add_and_subtract.cpp
@@ -0,0 +1,17 @@
+// program to prompt user for 2 numbers and display result of addition and subtraction on them.
+
+#include <iostream>
+
+int main()
+{
+	
+	std::cout << "Enter an integer: ";
+	int a{ };
+	std::cin >> a;
+
+	std::cout << "Enter another integer: ";
+	int b{ };
+	std::cin >> b;
+
+	return 0;
+}
