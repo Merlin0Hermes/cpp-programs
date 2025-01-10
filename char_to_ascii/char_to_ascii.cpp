e2bac56b48c1bb679e7ab0660765f0b01a5a4511
Merlin0Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-01-10 20:46:33 +0530

    add man code

--- char_to_ascii/char_to_ascii.cpp
+++ char_to_ascii/char_to_ascii.cpp
@@ -1,20 +1,15 @@
-// char_to_ascii.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Program to prompt the user for a single character and print its corresponding ASCII value
+
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char ch{};
+	std::cout << "Enter a single character: ";
+	std::cin >> ch;
+
+	std::cout << "You entered '" << ch << "' , which has ASCII code " << static_cast<int>(ch) << ".\n";
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
