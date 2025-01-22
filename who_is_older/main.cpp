4e3c05b83e1a1741bad9f119d379bb5f93a4bfe0
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-01-22 22:11:48 +0530

    add function to get name of person

--- who_is_older/main.cpp
+++ who_is_older/main.cpp
@@ -1,9 +1,29 @@
-#include <iostream>
+// Program to print who is older among two people. Prompts the user for name and age
 
-using namespace std;
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <iomanip>
+
+std::string get_name(std::string_view message);
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+    std::string person_one = get_name("Enter the name of person #1: ");
+    std::string person_two = get_name("Enter the name of person #2: ");
+
+    std::cout << person_one << "\n" << person_two;
+
+
     return 0;
+}
+
+std::string get_name(std::string_view message)
+{
+    std::cout << message;
+    std::string name;
+    std::getline(std::cin >> std::ws, name);
+
+    return name;
 }
