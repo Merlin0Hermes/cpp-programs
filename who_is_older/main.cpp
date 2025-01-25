3d7fec2f18b18754039db12ea55790a805bd4a53
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-01-25 10:57:10 +0530

    add function to print result

--- who_is_older/main.cpp
+++ who_is_older/main.cpp
@@ -7,6 +7,8 @@
 
 std::string get_name(std::string_view message);
 int get_age(std::string_view message);
+void print_result(std::string_view older_name, int old_age,
+                  std::string_view younger_name, int young_age);
 
 int main()
 {
@@ -17,10 +19,29 @@ int main()
     std::string person_two = get_name("Enter the name of person #2: ");
     int person_two_age = get_age("Enter the age of " + person_two + ": ");
 
+    if (person_one_age > person_two_age)
+    {
+        print_result(person_one, person_one_age, person_two, person_two_age);
+    }
+    else if (person_two_age > person_one_age)
+    {
+        print_result(person_two, person_two_age, person_one, person_one_age);
+    }
+    else
+    {
+        std::cout << "Both are the same age.\n";
+    }
 
 
     return 0;
 }
+
+void print_result(std::string_view older_name, int old_age,
+                  std::string_view younger_name, int young_age)
+{
+    std::cout << older_name << " (age " << old_age << ") is older than " << younger_name << " (age " << young_age << ").\n";
+}
+
 
 int get_age(std::string_view message)
 {
