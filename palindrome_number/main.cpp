10995393425abcb436c9956f615ae6ae7923179a
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-01-30 21:38:07 +0530

    add initial working version

--- palindrome_number/main.cpp
+++ palindrome_number/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <cstdint>
 
+int main()
+{
+    std::int64_t n{ }, reverse { }, original{ };
+    int digit{ };
+    std::cout << "Enter the number: ";
+    std::cin >> original;
+
+    n = original;
+
+    while (n != 0)
+    {
+        reverse = reverse * 10 + n % 10;
+        n /= 10;
+    }
+
+    if (original == reverse)
+        std::cout << original << " is a palindrome.\n";
+    else
+        std::cout << original << " is not a palindrome.\n";
+}
