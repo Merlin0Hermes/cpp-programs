b9ec2fd973329b9eafc2b4360eba764a88c25bcf
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-02-02 11:22:24 +0530

    add factorial function

---
+++ factorial/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdint>
+
+std::int64_t factorial(int n)
+{
+    if (n <= 0)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+
+int main()
+{
+    return 0;
+}
