3620377a4a930aaa897d07fe95963972cd8d573a
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-02-06 20:39:40 +0530

    add function to return a matrix with sum of 2 matrices

--- matrix/matrix_addition.cpp
+++ matrix/matrix_addition.cpp
@@ -73,3 +73,17 @@ int** get_matrix(int rows, int cols)
     return matrix;
 }
 
+int** add_matrices(int **matrix1, int **matrix2, int rows, int cols)
+{
+    int **result = create_matrix(rows, cols);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+}
+
