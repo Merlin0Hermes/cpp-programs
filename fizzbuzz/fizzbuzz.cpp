2d2312aaa094a9e2c69c954a6ad48df108b1db3a
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-04 13:54:49 +0530

    add 'pop' for number 7 and simplified if logic in fizzbuzz function

--- fizzbuzz/fizzbuzz.cpp
+++ fizzbuzz/fizzbuzz.cpp
@@ -1,3 +1,9 @@
+// program to print fizz buzz pop depending upon divisibility of numbers
+// fizz = divisible by 3
+// buzz = divisible by 5
+// pop = divisible by 7
+
+
 #include <iostream>
 
 void fizzbuzz(int n)
@@ -5,19 +11,20 @@ void fizzbuzz(int n)
     constexpr int limit{ 20 };
     for (int i{ 1 }; i <= limit; ++i)
     {
-        if ((i % 3 == 0) && (i % 5 == 0))
-        {
-            std::cout << "fizzbuzz\n";
-        }
-        else if (i % 3 == 0)
+
+        if (i % 3 == 0)
         {
             std::cout << "fizz\n";
         }
-        else if (i % 5 == 0)
+        if (i % 5 == 0)
         {
             std::cout << "buzz\n";
         }
-        else 
+        if (i % 7 == 0)
+        {
+            std::cout << "pop\n";
+        }
+        if (!(i % 3 == 0 || i % 5 == 0 || i % 7 == 0))
         {
             std::cout << i << "\n";
         }
