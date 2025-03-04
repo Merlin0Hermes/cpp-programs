ae1651d71a97f5da716c03dde8bcf627c95f6cfe
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-04 14:01:44 +0530

    simply if conditional with a new variable

--- fizzbuzz/fizzbuzz.cpp
+++ fizzbuzz/fizzbuzz.cpp
@@ -10,20 +10,23 @@ void fizzbuzz(int n)
 {
     for (int i{ 1 }; i <= n; ++i)
     {
-
+        bool printed{ false };
         if (i % 3 == 0)
         {
             std::cout << "fizz";
+            printed = true;
         }
         if (i % 5 == 0)
         {
             std::cout << "buzz";
+            printed = true;
         }
         if (i % 7 == 0)
         {
             std::cout << "pop";
+            printed = true;
         }
-        if (!(i % 3 == 0 || i % 5 == 0 || i % 7 == 0))
+        if (!(printed))
         {
             std::cout << i;
         }
