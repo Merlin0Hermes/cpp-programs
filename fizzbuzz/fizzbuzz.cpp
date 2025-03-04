bec3e2869305b38fedf58c212005b5e5700f76ed
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-04 13:42:07 +0530

    add fizzbuzz function basic functionality

--- fizzbuzz/fizzbuzz.cpp
+++ fizzbuzz/fizzbuzz.cpp
@@ -1,2 +1,30 @@
 #include <iostream>
 
+void fizzbuzz(int n)
+{
+    constexpr int limit{ 20 };
+    for (int i{ 1 }; i <= limit; ++i)
+    {
+        if ((i % 3 == 0) && (i % 5 == 0))
+        {
+            std::cout << "fizzbuzz\n";
+        }
+        else if (i % 3 == 0)
+        {
+            std::cout << "fizz\n";
+        }
+        else if (i % 5 == 0)
+        {
+            std::cout << "buzz\n";
+        }
+        else 
+        {
+            std::cout << i << "\n";
+        }
+    }
+}
+
+int main()
+{
+    fizzbuzz(5);
+}
