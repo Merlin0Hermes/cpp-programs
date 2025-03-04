3866c7c72e3fe60e7c0ec335fab499776513919b
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-04 19:41:20 +0530

    optimize for loop in isPrime function to compare from 3 to i * i, ignoring even numbers

--- isPrime/main.cpp
+++ isPrime/main.cpp
@@ -6,25 +6,23 @@
 
 bool isPrime(int x)
 {
-    if (x <= 1)
-    {
+    // 1 and negative numbers are not prime
+    if (x <= 1) 
         return false;
-    }
-    else if ((x != 2) && (x % 2 == 0))
-    {
-        return false;
-    }
 
-    for (int i{ 2 }; ((i * i) <= x); ++i)
+    // even numbers are not prime unless it is 2
+    else if ((x != 2) && (x % 2 == 0))
+        return false;
+
+    // test from 3 to i * i, ignoring even numbers with i += 2
+    for (int i{ 3 }; i * i <= x; i += 2)
     {
         if (x % i == 0)
         {
             return false;
         }
     }
-
     return true;
-    
 }
 
 int main()
