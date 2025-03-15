3f0d57f2503302761e12c9e2e4ca43d622141e19
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-15 19:28:53 +0530

    add overloaded >> operator for input

--- fraction_struct/main.cpp
+++ fraction_struct/main.cpp
@@ -1,8 +1,26 @@
 // prompts the user for two fractions and multiplies them and prints the result
 
-struct fraction
+#include <iostream>
+
+struct Fraction
 {
     int numerator{ };
     int denominator{ };
 };
+
+std::istream& operator>>(std::istream& in, Fraction& fraction)
+{
+    std::cout << "Enter a value for the numerator: ";
+    in >> fraction.numerator;
+
+    std::cout << "Enter a value for the denominator: ";
+    in >> fraction.denominator;
+
+    return in;
+}
+
+Fraction get_fraction()
+{
+
+}
 
