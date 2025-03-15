5b3b3695804c390c0bdd2818ba0d1abbc590bf26
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-15 18:51:10 +0530

    add function to read input from user

--- website_struct/main.cpp
+++ website_struct/main.cpp
@@ -9,12 +9,30 @@
 struct AdRevenue 
 {
     int ads_watched { }; // number of ads watched by users
-    int percent_clicked { }; // % of users that clicked on ads
-    int average_earnings { }; // average earnings per ad
+    double percent_clicked { }; // % of users that clicked on ads 
+    double average_earnings { }; // average earnings per ad
 };
+
+AdRevenue generate_struct()
+{
+    AdRevenue ar;
+
+    std::cout << "Enter the number of ads watched: ";
+    std::cin >> ar.ads_watched;
+
+    double temp;
+    std::cout << "Enter the percentage of users that clicked on ads: ";
+    std::cin >> temp;
+    ar.percent_clicked = temp / 100.0;
+
+    std::cout << "Enter the average earnings per ad: ";
+    std::cin >> ar.average_earnings;
+
+    return ar;
+}
 
 int main()
 {
-    
+
     return 0;
 }
