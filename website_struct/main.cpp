bf94b4aeffa6234d87e2d2a0bb1ae03f377237b6
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-15 18:44:50 +0530

    add struct AdRevenue

---
+++ website_struct/main.cpp
@@ -0,0 +1,20 @@
+// A struct that stores 
+// 1. How many ads were watched.
+// 2. What percentage of users clicked on an ad.
+// 3. The average earnings per clicked ad.
+// and prints the information
+
+#include <iostream>
+
+struct AdRevenue 
+{
+    int ads_watched { }; // number of ads watched by users
+    int percent_clicked { }; // % of users that clicked on ads
+    int average_earnings { }; // average earnings per ad
+};
+
+int main()
+{
+    
+    return 0;
+}
