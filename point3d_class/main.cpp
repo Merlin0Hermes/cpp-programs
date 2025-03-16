aaba0e587610c0e4eb7b0d873703667bff5d0bf1
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-16 19:36:00 +0530

    add class Point3d

--- point3d_class/main.cpp
+++ point3d_class/main.cpp
@@ -1,3 +1,33 @@
+// A class to store x, y, and z coordinates
+
+
+#include <iostream>
+
+
+class Point3d
+{
+public:
+    void setValues(int x, int y, int z)
+    {
+        m_x = x;
+        m_y = y;
+        m_z = z;
+    }
+
+    void print() const
+    {
+        std::cout << "<" << m_x << ", " << m_y << ", " << m_z << ">";
+    }
+
+private:
+    int m_x {};
+    int m_y {};
+    int m_z {};
+    
+};
+
+
+
 int main()
 {
     Point3d point;
