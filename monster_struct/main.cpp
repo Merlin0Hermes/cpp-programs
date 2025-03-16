94d9a559a67ab8efff1caf9b9e15b9c978464bb9
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-16 14:19:43 +0530

    add function to get string form of monster type enum

--- monster_struct/main.cpp
+++ monster_struct/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <string_view>
 
 
 enum class MonsterType
@@ -16,5 +17,29 @@ enum class MonsterType
 struct Monster
 {
     std::string name;
+    MonsterType type;
     int health;
 };
+
+
+std::string_view get_monster_type_string(MonsterType monster)
+{
+    using enum MonsterType;
+    switch (monster) 
+    {
+        case ogre: return "Ogre";
+        case dragon: return "Dragon";
+        case orc: return "Orc";
+        case giant_spider: return "Giant Spider";
+        case slime: return "Slime";
+
+        default: return "???";
+
+    }
+}
+
+
+void print_monster(Monster monster)
+{
+
+}
