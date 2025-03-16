a3e40c329627ee03d1afd55233005e41fcf23461
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-16 14:48:49 +0530

    add overloaded operator<< for printing Triad struct

--- triad_struct/main.cpp
+++ triad_struct/main.cpp
@@ -11,6 +11,13 @@ struct Triad
 template <typename T>
 Triad(T, T, T) -> Triad<T>;
 
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const Triad<T>& triad)
+{
+    out << "[" << triad.x << ", " << triad.y << ", " << triad.z << "]";
+    return out;
+}
+
 int main()
 {
 	Triad t1{ 1, 2, 3 }; // note: uses CTAD to deduce template arguments
