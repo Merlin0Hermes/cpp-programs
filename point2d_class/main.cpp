7c85bb0b41ff5f67603d67c9e27563aafa35d407
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-21 17:47:35 +0530

    update template code

--- point2d_class/main.cpp
+++ point2d_class/main.cpp
@@ -7,7 +7,7 @@ class Point2d
     
 public: 
     Point2d() = default;
-    
+
     Point2d(double x, double y)
     :m_x { x }
     ,m_y { y }
@@ -19,21 +19,30 @@ public:
         std::cout << "Point2d(" << m_x << ", " << m_y << ")\n";
     }
 
+
+    double distanceTo()
+    {
+        // TODO
+    }
+
 private:
     double m_x{ 0.0 };
     double m_y{ 0.0 };
 
 };
 
+#include <cmath>
+#include <iostream>
+
 int main()
 {
     Point2d first{};
     Point2d second{ 3.0, 4.0 };
 
-    // Point2d third{ 4.0 }; // should error if uncommented
-
     first.print();
     second.print();
+
+    std::cout << "Distance between two points: " << first.distanceTo(second) << '\n';
 
     return 0;
 }
