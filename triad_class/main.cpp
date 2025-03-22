2c5c45bb939471e198839b5111a32191b51afaa5
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-22 17:08:22 +0530

    add basic Triad template class

--- triad_class/main.cpp
+++ triad_class/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 #include <string>
 
+
+template <typename R, typename S, typename T>
+class Triad
+{
+public:
+    Triad(const R& x, const S& y, const T& z)
+    :m_first{ x }
+    ,m_second{ y }
+    ,m_third{ z }
+    { 
+    }
+
+    R first() { return m_first; }
+    S second() { return m_second; }
+    T third() { return m_third; }
+
+
+
+private:
+    R m_first{};
+    S m_second{};
+    T m_third{};
+
+};
+
+
+
+
 int main()
 {
 	Triad<int, int, int> t1{ 1, 2, 3 };
