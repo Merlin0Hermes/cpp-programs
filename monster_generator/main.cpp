cdc897c2c2e2367498e52e2a96e41151d4abc15e
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-26 13:17:05 +0530

    add constructor for class

--- monster_generator/main.cpp
+++ monster_generator/main.cpp
@@ -2,7 +2,6 @@
 
 class Monster
 {
-
     enum Type
     {
         dragon,
@@ -17,10 +16,19 @@ class Monster
            
     };
 
+    Monster(Type type, std::string_view name, std::string_view roar, int hit_points)
+    :m_type{ type }
+    ,m_name{ name }
+    ,m_roar{ roar }
+    ,m_hit_points{ hit_points }
+    { }
+
+
 private:
-    Type type{};
-    std::string name{"???"};
-    std::string roar{"???"};
-    int hit_points{};
+
+    Type m_type{};
+    std::string m_name{"???"};
+    std::string m_roar{"???"};
+    int m_hit_points{};
 
 };
