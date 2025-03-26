0f752d13eb5c1280a2940062d439f1fe7f2ecd5c
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-26 19:17:35 +0530

    add file

---
+++ print_vector/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+// Implement printArray() here
+
+int main()
+{
+    std::vector arr{ 4, 6, 7, 3, 8, 2, 1, 9 };
+
+    printArray(arr); // use function template to print array
+
+    return 0;
+}
