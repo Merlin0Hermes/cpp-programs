80f3dfe5312603f015d4e3e91df021f7fa8826e0
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-27 15:44:08 +0530

    move array checking to function template

--- name_in_vector/main.cpp
+++ name_in_vector/main.cpp
@@ -3,6 +3,18 @@
 #include <string>
 #include <string_view>
 
+template <typename T, typename U>
+bool is_value_in_array(const std::vector<T>& arr, const U& value)
+{
+    for (const auto& element: arr)
+    {
+        if (element == value)
+            return true;
+    }
+    return false;
+}
+
+
 int main()
 {
     static const std::vector<std::string_view> names { "Alex", "Betty", "Caroline", "Dave",
@@ -11,17 +23,8 @@ int main()
     std::cout << "Enter a name: ";
     std::string target{};
     std::cin >> target;
-
-    bool found { false };
-    for (auto name: names)
-    {
-        if (name == target)
-        {
-            found = true;
-            break;
-        }
-    }
-    if (found)
+    
+    if (is_value_in_array(names, target))
         std::cout << target << " was found.\n";
     else
         std::cout << target << " was not found.\n";
