b5e1ca2ee47bdb9eebf0b94475337297117fd2bb
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-29 19:11:24 +0530

    add output functions to provide detailed info about items

--- player_items/main.cpp
+++ player_items/main.cpp
@@ -1,3 +1,4 @@
+#include <ostream>
 #include <vector>
 #include <iostream>
 #include <cassert>
@@ -14,11 +15,48 @@ namespace Items
     };
 }
 
+std::string_view item_to_string(Items::Type item)
+{
+    using namespace Items;
+    switch (item) 
+    {
+        case health_potion: return "health potion";
+        case torch:         return "torch";
+        case arrow:         return "arrow";
+        
+        default:            return "???";
+    }
+}
+
+
+std::ostream& operator<<(std::ostream& out, Items::Type item)
+{
+    out << item_to_string(item);
+    return out;
+}
+
+
+void print_items(const std::vector<int>& inventory)
+{
+    for (int i{0}; i < inventory.size(); i++)
+    {
+        std::cout << "You have " << inventory[i] << " " << static_cast<Items::Type>(i);
+
+        if (inventory[i] > 1)
+            std::cout << "s";
+        
+        std::cout << "\n";
+    }
+}
+
+
 int main()
 {
     std::vector<int> inventory {1, 5, 10};
 
     assert(inventory.size() == Items::max_items);
+
+    print_items(inventory);
 
     int sum{};
     for (auto item: inventory)
