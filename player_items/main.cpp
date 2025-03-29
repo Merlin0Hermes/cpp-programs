e5afadfa91f34ab43552b6f4b0554c143f35c876
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-29 16:49:44 +0530

    add enum for item type

---
+++ player_items/main.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+namespace Items
+{
+    enum Type
+    {
+        health_potion,
+        torch,
+        arrow,
+        
+        max_items
+    };
+}
+
+int main()
+{
+    std::vector<int> inventory {1, 5, 10};
+
+}
