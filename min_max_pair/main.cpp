952a6ba44ad120143f73ec520a9d769b74fa685d
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-29 19:53:31 +0530

    add function to print min and max element details

--- min_max_pair/main.cpp
+++ min_max_pair/main.cpp
@@ -43,7 +43,10 @@ void print_vector(const std::vector<T>& arr)
 template <typename T>
 void print_min_max(const std::vector<T>& arr, std::size_t min_index, std::size_t max_index)
 {
+    print_vector(arr);
 
+    std::cout << "The min element has index " << min_index << " and value " << arr[min_index] << "\n";
+    std::cout << "The max element has index " << max_index << " and value " << arr[max_index] << "\n";
 
 }
 
@@ -51,6 +54,8 @@ int main()
 {
     std::vector v1 { 3, 8, 2, 5, 7, 8, 3 };
     std::vector v2 { 5.5, 2.7, 3.3, 7.6, 1.2, 8.8, 6.6 };
+
     print_vector(v1);
+    
 
 }
