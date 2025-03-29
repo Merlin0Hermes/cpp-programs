9d9f8b964f9d976b6e128801e3d0e1933f979a2c
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-29 19:56:13 +0530

    modify printing min and max function

--- min_max_pair/main.cpp
+++ min_max_pair/main.cpp
@@ -41,7 +41,7 @@ void print_vector(const std::vector<T>& arr)
 }
 
 template <typename T>
-void print_min_max(const std::vector<T>& arr, std::size_t min_index, std::size_t max_index)
+void print_min_max(const std::vector<T>& arr, std::pair<std::size_t, std::size_t>)
 {
     print_vector(arr);
 
@@ -55,7 +55,8 @@ int main()
     std::vector v1 { 3, 8, 2, 5, 7, 8, 3 };
     std::vector v2 { 5.5, 2.7, 3.3, 7.6, 1.2, 8.8, 6.6 };
 
-    print_vector(v1);
-    
+    print_min_max(v1, min_max(v1));
+    print_min_max(v2, min_max(v2));
+
 
 }
