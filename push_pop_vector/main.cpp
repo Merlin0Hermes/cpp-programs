05e2da7e740a5d60f50c7bb1ce61db442d82b006
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-29 13:14:22 +0530

    add function to pop from stack

--- push_pop_vector/main.cpp
+++ push_pop_vector/main.cpp
@@ -18,11 +18,21 @@ void print_stack(const std::vector<T>& stack)
     std::cout << ")\n";
 }
 
+
 template <typename T>
 void push(std::vector<T>& stack, T element)
 {
     std::cout << "Push " << element;
     stack.push_back(element);
+    print_stack(stack);
+}
+
+
+template <typename T>
+void pop(std::vector<T>& stack, T element)
+{
+    std::cout << "Pop " << element;
+    stack.pop_back(element);
     print_stack(stack);
 }
 
