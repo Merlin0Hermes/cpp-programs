dd25988866b6f3ffcd22705df8cb5ca6fdbc6d6b
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-29 21:43:14 +0530

    add word list

--- c++man/main.cpp
+++ c++man/main.cpp
@@ -1,2 +1,12 @@
 // A game similar to hangman
 
+#include <vector>
+#include <string_view>
+
+namespace WordList
+{
+    const std::vector<std::string_view> words {"mystery", "broccoli","account", "almost", "spaghetti", "opinion", "beautiful",
+        "distance", "luggage"};
+    
+}
+
