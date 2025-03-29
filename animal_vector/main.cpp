545aa0c080ff57817812e47b7a018b252d9ceaf3
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-29 12:51:10 +0530

    add main() code

--- animal_vector/main.cpp
+++ animal_vector/main.cpp
@@ -1,3 +1,7 @@
+#include <vector>
+#include <iostream>
+#include <cassert>
+
 namespace Animals
 {
     enum Animals
@@ -8,6 +12,19 @@ namespace Animals
         elephant,
         duck,
         snake,
+
+        max_animals
     };
 }
 
+
+int main()
+{
+    std::vector legs {2, 4, 4, 4, 2, 0}; // number of legs of each animal
+
+    assert(legs.size() == Animals::max_animals);
+
+    std::cout << "Elephant has " << legs[Animals::elephant] << " legs.\n";
+
+    return 0;
+}
