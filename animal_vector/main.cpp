b045f4c8a59dcef964bdef68b820df7620c15fb9
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-29 12:52:44 +0530

    move vector to inside namespace and make it const

--- animal_vector/main.cpp
+++ animal_vector/main.cpp
@@ -15,16 +15,18 @@ namespace Animals
 
         max_animals
     };
+
+    const std::vector legs {2, 4, 4, 4, 2, 0};
 }
 
 
 int main()
 {
-    std::vector legs {2, 4, 4, 4, 2, 0}; // number of legs of each animal
+     // number of legs of each animal
 
-    assert(legs.size() == Animals::max_animals);
+    assert(Animals::legs.size() == Animals::max_animals);
 
-    std::cout << "Elephant has " << legs[Animals::elephant] << " legs.\n";
+    std::cout << "Elephant has " << Animals::legs[Animals::elephant] << " legs.\n";
 
     return 0;
 }
