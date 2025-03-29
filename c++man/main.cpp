c96b20b2d34ae313ae51d050db708c474e9c3247
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-29 21:48:20 +0530

    add function to generate random word

--- c++man/main.cpp
+++ c++man/main.cpp
@@ -1,7 +1,9 @@
 // A game similar to hangman
 
+#include <iostream>
 #include <vector>
 #include <string_view>
+#include "../Random.h"
 
 namespace WordList
 {
@@ -10,3 +12,16 @@ namespace WordList
     
 }
 
+std::string_view random_word()
+{
+    return WordList::words[Random::get<std::size_t>(0, WordList::words.size() - 1)];
+}
+
+
+int main()
+{
+    std::cout << "Welcome to C++man (a variant of Hangman)\n";
+    std::cout << "To win: guess the word.  To lose: run out of pluses.\n";
+
+    std::cout << "The word is: " << random_word() << "\n";
+}
