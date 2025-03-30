68272fccbfe5a9b2c5c2f85f6fcbb3db10a3ae9d
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-30 14:47:49 +0530

    update display function to display wrong guesses

--- c++man/main.cpp
+++ c++man/main.cpp
@@ -28,30 +28,61 @@ public:
     void set_guessed(char letter)
     {
         m_letters_guessed[(letter % 32) - 1] = true;
+        set_wrong_guess(letter);
     }
+
     bool guessed(char letter) const
     {
         return m_letters_guessed[(letter % 32) - 1];
+    }
+
+    int guess_remaining() const { return m_guesses; }
+    std::vector<char> get_wrong_guesses() const { return m_wrong_guesses; }
+
+    void set_wrong_guess(char letter)
+    {
+        for (auto a: m_word)
+        {
+            if (a == letter)
+                return;
+        }
+        --m_guesses;
+        m_wrong_guesses.push_back(letter);
+
     }
     const std::vector<bool>& letters_guessed() const { return m_letters_guessed; }
 
 private:
     std::vector<bool> m_letters_guessed {std::vector<bool>(26)};
     std::string m_word{ WordList::random_word() };
+    int m_guesses { 6 };
+    std::vector<char> m_wrong_guesses { };
 };
 
 
-void display_state(const Session& session)
+
+void display_state(const Session& s)
 {
 
     std::cout << "\nThe word: ";
-    for (const auto& a: session.word())
+    for (const auto& a: s.word())
     {
-        if (session.guessed(a))
+        if (s.guessed(a))
             std::cout << a;
         else
             std::cout << "_";
     }
+
+    std::cout << "\tWrong guesses: ";
+    for (int i {0}; i < s.guess_remaining(); ++i)
+    {
+        std::cout << "+";
+    }
+    for (const auto& a: s.get_wrong_guesses())
+    {
+        std::cout << a;
+    }
+
     std::cout << "\n";
 }
 
