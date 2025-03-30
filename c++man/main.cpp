436609bcd627f306fdbf7d3374ab21dbb67788d9
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-30 17:32:50 +0530

    format class and functions

--- c++man/main.cpp
+++ c++man/main.cpp
@@ -23,54 +23,72 @@ namespace WordList
 class Session
 {
 public:
- 
+
+    Session(int max=6): m_guesses { max } 
+    { }
+
+    // getters
     std::string_view word() const { return m_word; }
-    void set_guessed(char letter)
-    {
-        m_letters_guessed[(letter % 32) - 1] = true;
-        set_wrong_guess(letter);
-    }
-
-    bool guessed(char letter) const
-    {
-        return m_letters_guessed[(letter % 32) - 1];
-    }
-
     int guess_remaining() const { return m_guesses; }
     std::vector<char> get_wrong_guesses() const { return m_wrong_guesses; }
-
-    void set_wrong_guess(char letter)
-    {
-        for (auto a: m_word)
-        {
-            if (a == letter)
-                return;
-        }
-        --m_guesses;
-        m_wrong_guesses.push_back(letter);
-
-    }
     const std::vector<bool>& letters_guessed() const { return m_letters_guessed; }
 
+    // functions to update and return guessed letters
+    void set_guessed(char letter); 
+    bool guessed(char letter) const;
+    
+
 private:
-    std::vector<bool> m_letters_guessed {std::vector<bool>(26)};
     std::string m_word{ WordList::random_word() };
-    int m_guesses { 6 };
+    std::vector<bool> m_letters_guessed {std::vector<bool>(26)};
+    int m_guesses {6};
     std::vector<char> m_wrong_guesses { };
+
+    void set_wrong_guess(char letter); // helper function for set_guessed(char letter)
 };
 
+
+bool Session::guessed(char letter) const
+{
+    return m_letters_guessed[(letter % 32) - 1];
+}
+
+
+void Session::set_guessed(char letter)
+{
+    m_letters_guessed[(letter % 32) - 1] = true;
+    set_wrong_guess(letter);
+}
+
+
+void Session::set_wrong_guess(char letter)
+{
+    for (auto a: m_word)
+    {
+        if (a == letter)
+            return;
+    }
+    --m_guesses;
+    m_wrong_guesses.push_back(letter);
+}
 
 
 void display_state(const Session& s)
 {
 
     std::cout << "\nThe word: ";
+
+    bool word_known { true };
     for (const auto& a: s.word())
     {
         if (s.guessed(a))
             std::cout << a;
         else
+        {
             std::cout << "_";
+            word_known = false;
+        }
+            
     }
 
     std::cout << "\tWrong guesses: ";
@@ -83,11 +101,6 @@ void display_state(const Session& s)
         std::cout << a;
     }
     std::cout << "\n";
-
-    if (s.guess_remaining() <= 0)
-    {
-        std::cout << "You lost!  The word was: " << s.word() << "\n";
-    }
 
 }
 
