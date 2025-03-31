646d639033221f3b8f5f984ff859049535a20fe6
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-03-31 10:50:14 +0530

    make array of Items

--- c++man/item_struct/main.cpp
+++ c++man/item_struct/main.cpp
@@ -10,5 +10,13 @@ struct Item
 
 int main()
 {
+    std::array items {
+        Item{"sword", 5},              
+        Item{"dagger", 3},
+        Item{"club", 2},
+        Item{"spear", 7}
+    };
+
     
+    return 0;
 }
