8a0e22bb269ca8c2c6c7e69b64db877f67eb2dc6
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-05 14:18:15 +0530

    add function to print string in reverse

--- print_string/main.cpp
+++ print_string/main.cpp
@@ -3,8 +3,15 @@
 #include <iostream>
 #include <cassert>
 
-void print_str(const char * str)
+void print_str_reverse(const char * str);
+
+void print_str(const char * str, bool reverse = false)
 {
+    if (reverse)
+    {
+        print_str_reverse(str);   
+        return;
+    }
     while (*str != '\0')
     {
         std::cout << *str;
@@ -12,9 +19,17 @@ void print_str(const char * str)
     }
 }
 
+void print_str_reverse(const char * str)
+{
+    const char * ptr{str};
+    for (; *ptr != '\0'; ++ptr)
+        ;
+    while (--ptr != (str - 1))
+        std::cout << *ptr;
+}
 
 int main()
 {
-    print_str("Hello, world!\n");
+    print_str("\nHello, world!", true);
     return 0;
 }
