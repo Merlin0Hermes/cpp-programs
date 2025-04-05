0b56fa7b66ac4430a99d43ca278587c446b71a2c
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-05 14:03:07 +0530

    add printing function for C-style strings

--- print_string/main.cpp
+++ print_string/main.cpp
@@ -1,2 +1,14 @@
 // program to print C-style string
 
+#include <cstddef>
+#include <iostream>
+#include <cassert>
+
+void print_str(char * str)
+{
+    assert(*str != '\0' && "string cannot be empty.");
+    for (char* ptr{str}; *ptr != '\n'; ptr++)
+    {
+        std::cout << *ptr;
+    }
+}
