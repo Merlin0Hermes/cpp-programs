e880f165ba9ed5fa565d469273a6554c73230101
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-13 17:43:50 +0530

    add basic player class and function to play blackjack

--- blackjack/main.cpp
+++ blackjack/main.cpp
@@ -1,1 +1,36 @@
 // simplified version of blackjack game
+
+#include <iostream>
+#include "deck.h"
+#include "../Random.h"
+
+
+struct Player
+{
+    int score{0};
+};
+
+bool play_blackjack()
+{
+    Deck deck {};
+    deck.shuffle();
+
+    Player dealer { deck.deal_card().value() }; // deal a card for dealer
+    
+    Player user { deck.deal_card().value() + deck.deal_card().value() }; // deal two cards for user
+
+    std::cout << "The dealer is showing: " << dealer.score << "\n";
+    std::cout << "You have score: " << user.score << "\n";
+}
+
+
+int main()
+{
+    bool won { play_blackjack() };
+
+    if (won)
+        std::cout << "You win!\n";
+    else
+        std::cout << "You lose!\n";
+
+}
