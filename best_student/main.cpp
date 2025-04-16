981e046c17a9e424968d4994a3e092ee606c43a0
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-16 18:50:17 +0530

    add array of students and lambda function

--- best_student/main.cpp
+++ best_student/main.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <array>
 
 struct Student
 {
@@ -8,3 +9,25 @@ struct Student
 };
 
 
+int main()
+{
+    std::array<Student, 8> arr{
+        { { "Albert", 3 },
+          { "Ben", 5 },
+          { "Christine", 2 },
+          { "Dan", 8 }, // Dan has the most points (8).
+          { "Enchilada", 4 },
+          { "Francis", 1 },
+          { "Greg", 3 },
+          { "Hagrid", 5 } }
+      };
+
+    auto func {
+    [] (const Student& a, const Student& b)
+    {
+        return a.points < b.points;
+    } 
+    };
+
+
+}
