1a21170129f69a3c3071f9efa95060344bc8a574
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-17 13:25:54 +0530

    change finding and erasing elements to a function

--- square_numbers/main.cpp
+++ square_numbers/main.cpp
@@ -54,35 +54,42 @@ std::vector<T> generate_numbers(T start, std::size_t n, U mult)
     return vec;
 }
 
+template <typename T>
+bool find_and_erase(std::vector<T>& vec, T element)
+{
+    auto found { std::find(vec.begin(), vec.end(), element) };
+
+    if (found == vec.end())
+        return false;
+    vec.erase(found);
+    return true;
+}
+
 
 void play_game(int start, int n)
 {
     int mult { Random::get(Settings::min_mult, Settings::max_mult) };
 
     std::vector squares { generate_numbers(start, n, mult) };
-
     std::cout << "I generated " << n << " square numbers. Do you know what each number is after multiplying it by " << mult << "?\n";
 
     while (true)
     {
         int guess { get_num() };
-        auto found { std::find(squares.begin(), squares.end(), guess) };
 
-        if (found == squares.end())
+        if (find_and_erase(squares, guess))
         {
-            std::cout << guess << " is wrong! Try " << closest_element(squares, guess) << " next time.\n";
-            return;
-        }
-        else 
-        {
-            squares.erase(found);
             if (squares.empty())
                 break;
             std::cout << "Nice! " << squares.size() << " number(s) left.\n";
         }
+        else 
+        {
+            std::cout << guess << " is wrong! Try " << closest_element(squares, guess) << " next time.\n";
+            return;
+        }
     }
     std::cout << "Nice! You found all numbers, good job!\n";
-
 }
 
 
