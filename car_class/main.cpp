f36f63ff5b9032abcd72fd07cf42c42045ba4a91
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-17 21:05:21 +0530

    add overloaded operator< for Car class

--- car_class/main.cpp
+++ car_class/main.cpp
@@ -18,8 +18,10 @@ public:
 
     friend bool operator== (const Car& c1, const Car& c2);
     friend bool operator!= (const Car& c1, const Car& c2);
+    friend bool operator<  (const Car& c1, const Car& c2);
     
     friend std::ostream& operator<<(std::ostream& out, const Car& car);
+
 };
 
 bool operator== (const Car& c1, const Car& c2)
@@ -32,6 +34,13 @@ bool operator!= (const Car& c1, const Car& c2)
 {
     return (c1.m_make != c2.m_make ||
             c1.m_model != c2.m_model);
+}
+
+bool operator< (const Car& c1, const Car& c2)
+{
+    if (c1.m_make == c2.m_make)
+        return c1.m_model < c2.m_model;
+    return c1.m_make < c2.m_make;
 }
 
 std::ostream& operator<<(std::ostream& out, const Car& car)
