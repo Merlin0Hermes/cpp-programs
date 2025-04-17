54819122e4673cad78a165b0ddc721f0edb372d2
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-17 20:53:26 +0530

    add template code

---
+++ car_class/main.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string_view>
+
+class Car
+{
+private:
+    std::string m_make;
+    std::string m_model;
+
+public:
+    Car(std::string_view make, std::string_view model)
+        : m_make{ make }, m_model{ model }
+    {
+    }
+
+    friend bool operator== (const Car& c1, const Car& c2);
+    friend bool operator!= (const Car& c1, const Car& c2);
+};
+
+bool operator== (const Car& c1, const Car& c2)
+{
+    return (c1.m_make == c2.m_make &&
+            c1.m_model == c2.m_model);
+}
+
+bool operator!= (const Car& c1, const Car& c2)
+{
+    return (c1.m_make != c2.m_make ||
+            c1.m_model != c2.m_model);
+}
+
+int main()
+{
+  std::vector<Car> cars{
+    { "Toyota", "Corolla" },
+    { "Honda", "Accord" },
+    { "Toyota", "Camry" },
+    { "Honda", "Civic" }
+  };
+
+  std::sort(cars.begin(), cars.end()); // requires an overloaded operator<
+
+  for (const auto& car : cars)
+    std::cout << car << '\n'; // requires an overloaded operator<<
+
+  return 0;
+}
