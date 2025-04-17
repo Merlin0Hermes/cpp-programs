d1a3d8430a7d4fd54d042295ec366c5369045e4c
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-17 11:55:35 +0530

    add function to get a number

--- square_numbers/main.cpp
+++ square_numbers/main.cpp
@@ -1,9 +1,30 @@
+#include <cctype>
 #include <iostream>
+#include <limits>
 
+
+int get_num()
+{
+    while (true)
+    {
+        int num{};
+        std::cin >> num;
+
+        if (!std::cin)
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
+
+        return num;
+    }
+}
 
 
 int main()
 {
+    std::cout << get_num();
 
     return 0;
 }
