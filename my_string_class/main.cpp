419f538fd07f70ab11732c1480c07f0afc850fbd
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-18 12:22:32 +0530

    add substring operation using operator()

--- my_string_class/main.cpp
+++ my_string_class/main.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <iostream>
 #include <string>
 #include <string_view>
@@ -7,14 +8,22 @@ class MyString
 public:
     MyString(std::string_view str): m_str {str} { }
 
-    friend std::ostream& operator<<(std::ostream& out, const MyString& str);
+    friend std::ostream& operator<< (std::ostream& out, const MyString& str);
+    std::string operator() (int index, int length);
     
 private:
     std::string m_str{};
 };
 
+std::string MyString::operator() (int index, int length)
+{
+    assert(index >= 0 && length >= 0);
+    return m_str.substr(index, length);
+}
 
-std::ostream& operator<<(std::ostream& out, const MyString& str)
+
+
+std::ostream& operator<< (std::ostream& out, const MyString& str)
 {   
     return out << str.m_str;
 }
