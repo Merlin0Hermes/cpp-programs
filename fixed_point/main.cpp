72f1ba563abb03fb4e53ee835c47cfa24184a760
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-18 19:20:22 +0530

    add main code

--- fixed_point/main.cpp
+++ fixed_point/main.cpp
@@ -1,2 +1,29 @@
 #include <iostream>
+#include <cassert>
 
+
+
+int main()
+{
+	FixedPoint2 a{ 34, 56 };
+	std::cout << a << '\n';
+	std::cout << static_cast<double>(a) << '\n';
+	assert(static_cast<double>(a) == 34.56);
+
+	FixedPoint2 b{ -2, 8 };
+	assert(static_cast<double>(b) == -2.08);
+
+	FixedPoint2 c{ 2, -8 };
+	assert(static_cast<double>(c) == -2.08);
+
+	FixedPoint2 d{ -2, -8 };
+	assert(static_cast<double>(d) == -2.08);
+
+	FixedPoint2 e{ 0, -5 };
+	assert(static_cast<double>(e) == -0.05);
+
+	FixedPoint2 f{ 0, 10 };
+	assert(static_cast<double>(f) == 0.1);
+
+	return 0;
+}
