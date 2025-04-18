2138b91b435e7691b38876fe9e0c99a0bea1a5e3
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-18 18:34:43 +0530

    add utility function deep_copy() for use in copy constructor and operator=

--- int_array_class/main.cpp
+++ int_array_class/main.cpp
@@ -8,33 +8,25 @@
 class IntArray
 {
 public:
-    IntArray(int length)
+    IntArray(int length) // constructor
     :m_length{ length }
     {
         assert(length > 0);
         m_array = {new int[length]};
     }
 
-    IntArray(const IntArray& source)
+    IntArray(const IntArray& source) // copy constructor
     {
         if (this == &source)
             return;
-
-        delete[] m_array;
-        
-        assert(source.m_length > 0);
-
-        m_length = source.m_length;
-        m_array = {new int[m_length]};
-
-        for (std::size_t i {0}; i < static_cast<std::size_t>(m_length); ++i)
-            m_array[i] = source.m_array[i];
+        deep_copy(source);
     }
 
-    ~IntArray()
+    ~IntArray() // destructor
     {
         delete[] m_array;
         m_array = nullptr;
+        m_length = 0;
     }
 
     const int& operator[](int index) const;
@@ -47,8 +39,21 @@ private:
     int* m_array{ nullptr };
     int m_length{};
 
+    void deep_copy(const IntArray& source);
 
 };
+
+void IntArray::deep_copy(const IntArray& source)
+{
+    delete[] m_array;
+    assert(source.m_length > 0);
+
+    m_length = source.m_length;
+    m_array = {new int[m_length]};
+
+    for (std::size_t i {0}; i < static_cast<std::size_t>(m_length); ++i)
+        m_array[i] = source.m_array[i];
+}
 
 
 std::ostream& operator<<(std::ostream& out, IntArray arr)
@@ -76,20 +81,10 @@ int& IntArray::operator[](int index)
     return const_cast<int&>(std::as_const(*this)[index]);
 }
 
-
 IntArray& IntArray::operator=(const IntArray& source)
 {
-    if (this == &source)
-        return *this;
-    assert(source.m_length > 0);
-
-    delete[] m_array;
-
-    m_length = source.m_length;
-    m_array = {new int[m_length]};
-
-    for (int i {0}; i < m_length; ++i)
-        m_array[i] = source.m_array[i];
+    if (this != &source)
+        deep_copy(source);
     return *this;
 }
 
