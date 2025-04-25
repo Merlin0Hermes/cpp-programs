dccd46f32a15cd452f445a24e86c17e41eb64496
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-25 16:18:39 +0530

    add class Fruit and derived class Apple

--- fruit_inheritance/main.cpp
+++ fruit_inheritance/main.cpp
@@ -1,6 +1,36 @@
 // A fruit class and derived classes
 
 #include <iostream>
+#include <string>
+#include <string_view>
+
+class Fruit
+{
+public:
+    Fruit(std::string_view name, std::string_view color)
+    :m_name{name}
+    ,m_color{color}
+    {
+    }
+
+private:
+    std::string m_name{};
+    std::string m_color{};
+};
+
+class Apple: Fruit
+{
+public:
+    Apple(std::string_view name, std::string_view color, double fiber)
+    :Fruit{name, color}
+    ,m_fiber{fiber}
+    {
+    }
+
+private:
+    double m_fiber{};
+};
+
 
 int main()
 {
