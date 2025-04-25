52fdd841e18458bd3f9849fd300cafbe6afe83c3
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-25 16:11:54 +0530

    add main() code

--- fruit_inheritance/main.cpp
+++ fruit_inheritance/main.cpp
@@ -1,2 +1,14 @@
 // A fruit class and derived classes
 
+#include <iostream>
+
+int main()
+{
+	const Apple a{ "Red delicious", "red", 4.2 };
+	std::cout << a << '\n';
+
+	const Banana b{ "Cavendish", "yellow" };
+	std::cout << b << '\n';
+
+	return 0;
+}
