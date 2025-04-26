2e51ff0945fc5a27ca72746878a7a49ac14f5128
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-26 19:47:40 +0530

    add Fruit base class

--- fruit_inheritance2/main.cpp
+++ fruit_inheritance2/main.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
+#include <string>
+#include <string_view>
+
+
+class Fruit
+{
+public:
+
+    Fruit(std::string_view name, std::string_view color)
+    :m_name{name}
+    ,m_color{color}
+    {
+    }
+
+private:
+    std::string m_name{};
+    std::string m_color{};
+};
 
 int main()
 {
