6cc7edf588360fe86c75d141bf8f35eb2fa8e549
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-26 21:08:00 +0530

    add has_won() function: returns true if player has reached a specified level

--- monster_game/Player.h
+++ monster_game/Player.h
@@ -11,7 +11,10 @@ namespace  PlayerDefault
     constexpr int attack_damage{1};
     constexpr int gold{0};
     constexpr int level{1};
+    constexpr int winning_level{20};
 }
+
+
 class Player: public Creature
 {
 private:
@@ -25,11 +28,16 @@ public:
     {
     }
 
-    void level_up()
+    int level() const {return m_level; }
+
+    void level_up() // increase player's level and attack damage
     {
         ++m_level;
         ++m_attack_damage;
     }
+
+    bool has_won(){ return m_level == PlayerDefault::winning_level; }
+
 };
 
 #endif
