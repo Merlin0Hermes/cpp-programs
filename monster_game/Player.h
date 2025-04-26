51e07238a7d55250d05b4880a428d5c4f4fb1a64
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-26 20:59:59 +0530

    add Player class

---
+++ monster_game/Player.h
@@ -0,0 +1,27 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "Creature.h"
+
+namespace  PlayerDefault 
+{
+    constexpr std::string_view name{"Unknown"};
+    constexpr char symbol{'@'};
+    constexpr int health{10};
+    constexpr int attack_damage{1};
+    constexpr int gold{0};
+}
+class Player: public Creature
+{
+private:
+    int m_level{1};
+public:
+    Player(std::string_view name = PlayerDefault::name, char symbol = PlayerDefault::symbol,
+            int health = PlayerDefault::health, int attack_damage = PlayerDefault::attack_damage,
+            int gold = PlayerDefault::gold)
+    :Creature(name, symbol, health, attack_damage, gold)
+    {
+    }
+};
+
+#endif
