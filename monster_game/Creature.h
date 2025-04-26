8bc5fef837f137f1caa379fba8778eb838f1cc20
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-26 20:42:07 +0530

    add utility functions to reduce health, add gold, and check if Creature is dead

--- monster_game/Creature.h
+++ monster_game/Creature.h
@@ -7,11 +7,17 @@
 class Creature
 {
 public:
+    // getters
     const std::string& name() const { return m_name; }
     char symbol() const { return m_symbol; }
     int health() const { return m_health; }
     int attack_damage() const { return m_attack_damage; }
     int gold() const { return m_gold; } 
+
+
+    void reduce_health(int n) {m_health -= n; } // reduces creature's health by n 
+    bool is_dead() const { return m_health <= 0; } // monster is dead if health <= 0
+    void add_gold(int gold) { m_gold += gold; } // adds gold
 
 private:
     std::string m_name{};
