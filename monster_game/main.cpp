c5cf5d734d8d7df7dab96695d386b03242de769c
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-27 21:31:25 +0530

    add game logic

--- monster_game/main.cpp
+++ monster_game/main.cpp
@@ -3,22 +3,72 @@
 // implemented using inheritance
 
 #include <iostream>
+#include <cctype>
 
+#include "../Random.h"
 #include "Creature.h"
 #include "Player.h"
 #include "Monster.h"
 
 
-void fight_monsters(Player& p)
+void attack_monster(Player& p, Monster& m)
+{
+	std::cout << "You hit the " << m.name() << " for " << m.attack_damage() << " damage.\n";
+	m.reduce_health(p.attack_damage());
+}
+
+
+void attack_player(Player& p, const Monster&m)
+{
+	std::cout << "The " << m.name() << " hit you for " << m.attack_damage() << " damage.\n";
+	p.reduce_health(m.attack_damage());
+}
+
+
+void fight_monster(Player& p)
 {
 	Monster m{Monster::random_monster()};
 
 	std::cout << "You have encountered a " << m.name() << "(" << m.symbol() << ").\n";
 
-	std::cout << "(R)un or (F)ight: ";
-	char input{};
-	std::cin >> input;
+	while (true)
+	{
+		std::cout << "(R)un or (F)ight: ";
+		char input{};
+		std::cin >> input;
 
+		if (std::toupper(input) == 'F')
+		{
+			attack_monster(p, m);
+			attack_player(p, m);
+		}
+		else if (std::toupper(input) == 'R')
+		{
+			if (Random::get(0, 1) == 0)
+			{
+				std::cout << "You failed to flee.\n";
+				attack_player(p, m);
+			}
+			else
+			{
+				std::cout << "You successfully fled.\n";
+				return;
+			}
+		}
+
+		if (m.is_dead())
+		{
+			std::cout << "You killed the " << m.name() << ".\n";
+			std::cout << "You are now level " << p.level() << ".\n";
+			std::cout << "You found " << m.gold() << "gold." << "\n";
+			p.add_gold(m.gold());
+			p.level_up();
+			return;
+		}
+
+		if (p.has_won() || p.is_dead())
+			return;
+	}
 	
 }
 
@@ -31,10 +81,20 @@ int main()
 	Player player {name};
 	std::cout << "Welcome, " << name << "\n";
 
-	while (true)
+	while (!player.is_dead() && !player.has_won())
 	{
-		
-		fight_monsters(player);
+		fight_monster(player);
+	}
+
+	if (player.is_dead())
+	{
+		std::cout << "You died at level " << player.level() << " with " << player.gold() << " gold.\n";
+		std::cout << "Too bad you can’t take it with you!\n";
+	}
+
+	if (player.has_won())
+	{
+		std::cout << "You win!\n";
 	}
 
 
