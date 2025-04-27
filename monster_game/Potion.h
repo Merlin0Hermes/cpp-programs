dc0e82d18ccc71c2ecbfd344cd935879466f8ce5
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-27 22:38:45 +0530

    implement Potion class for 3 type of potions

--- monster_game/Potion.h
+++ monster_game/Potion.h
@@ -1,8 +1,52 @@
 #ifndef POTION_H
 #define POTION_H
 
+#include <string>
+#include "../Random.h"
+
 class Potion
 {
+public:
+    enum Type
+    {
+        health,
+        strength,
+        poison,
+
+        max_types
+    };
+
+    enum Size
+    {
+        small,
+        medium,
+        large,
+
+        max_size
+    };
+
+    Potion(Type type, Size size)
+    :m_type{type}
+    ,m_size{size}
+    {
+    }
+
+    const std::string& type() const
+    {
+        static std::string name[] {"health", "strength", "poison"};
+        return name[m_type];
+    }
+
+    static Potion random_potion() 
+    {
+        Type rand_type {static_cast<Type>(Random::get(0, max_types))};
+        Size rand_size {static_cast<Size>(Random::get(0, max_size))};
+        return Potion(rand_type, rand_size);
+    }
+
+private:
+    Type m_type{};
+    Size m_size{};
 
 };
 
