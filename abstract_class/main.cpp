aa1b16f7df02be9941370576654843513a9aa963
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-05-01 19:18:08 +0530

    add file

---
+++ abstract_class/main.cpp
@@ -0,0 +1,18 @@
+
+#include <ostream>
+
+
+class Shape
+{
+public:
+    virtual std::ostream& print(std::ostream& out) const = 0;
+
+    friend std::ostream& operator<<(std::ostream& out, const Shape& shape)
+    {
+        return shape.print(out);
+    }
+
+    virtual ~Shape() = default;
+};
+
+
