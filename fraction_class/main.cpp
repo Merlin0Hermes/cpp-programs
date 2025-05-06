a158184bcf2806c029c06820da0f6a51e3f688c5
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-05-06 18:02:55 +0530

    update main() code

--- fraction_class/main.cpp
+++ fraction_class/main.cpp
@@ -120,14 +120,16 @@ bool operator>=(const Fraction& f1, const Fraction& f2)
 
 int main()
 {
-	Fraction f1{ 3, 2 };
-	Fraction f2{ 5, 8 };
+    std::cout << "Enter the numerator: ";
+    int num{};
+    std::cin >> num;
 
-	std::cout << f1 << ((f1 == f2) ? " == " : " not == ") << f2 << '\n';
-	std::cout << f1 << ((f1 != f2) ? " != " : " not != ") << f2 << '\n';
-	std::cout << f1 << ((f1 < f2) ? " < " : " not < ") << f2 << '\n';
-	std::cout << f1 << ((f1 > f2) ? " > " : " not > ") << f2 << '\n';
-	std::cout << f1 << ((f1 <= f2) ? " <= " : " not <= ") << f2 << '\n';
-	std::cout << f1 << ((f1 >= f2) ? " >= " : " not >= ") << f2 << '\n';
-	return 0;
+    std::cout << "Enter the denominator: ";
+    int denom{};
+    std::cin >> denom;
+
+    Fraction fraction {num, denom};
+
+    std::cout << fraction << "\n";
+
 }
